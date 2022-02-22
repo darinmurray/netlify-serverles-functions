d81731898aa607a0df18b8df272dacba252cc905
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-22 15:22:26 -0600

    no idea what's happening

--- public/script.js
+++ public/script.js
@@ -1,49 +1,20 @@
 window.onload = function() {
 
-  // var TEST_KEY = config.TEST_KEY;
-
-  const message = document.getElementById("message")
-  document.getElementById("button").addEventListener("click", () => {
-    // console.log("hello", TEST_KEY)
-    randosmessage = getRandomInt(1000, 10000)+randoAlpha+getRandomInt(1000, 10000)+randoAlpha
-    fetch("/.netlify/functions/hello-world?name=" + randosmessage)
-      .then((data) => data.json())
-      .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
-  })
+  // netlify = "affectionate-poincare-72fa68.netlify.app"
+  // localHost = "localhost"
+  // liveServer = "127.0.0.1"
 
 
 console.log("hostname is: ", window.location.hostname );
-
-
-const getRandomDadJoke = async () => {
-  /* const url = "https://icanhazdadjoke.com/";
-  const jokeStream = await fetch(url, {
-    headers: {
-      Accept: "application/json"
-    }
-  }); */
-  const url = "/.netlify/functions/jokes";
-  const jokeStream = await fetch(url);
-  const jsonJoke = await jokeStream.json();
-  const joke = jsonJoke.joke;
-  return joke;
-};
-
-const displayJoke = (joke) => {
-  const h1 = document.querySelector("h1");
-  h1.textContent = joke;
-};
-
-const refreshJoke = async () => {
-  const joke = await getRandomDadJoke();
-  displayJoke(joke);
-};
-
-// load 1st joke
-// refreshJoke();
-
-// setInterval(refreshJoke, 3000);
-
+  const message = document.getElementById("message")
+  document.getElementById("button").addEventListener("click", () => {
+    // console.log("hello", TEST_KEY)
+    randosmessage = getRandomInt(1000, 10000)
+    // fetch("/.netlify/functions/hello-world?name=" + randosmessage)
+    fetch("./functions/hello-world?name=" + randosmessage)
+      .then((data) => data.json())
+      .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
+  })
 
 
 
