92e9ab14a354aa5e2450296f689538e7f28df65d
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-22 15:22:42 -0600

    no idea what's happening

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -4,11 +4,21 @@
   const { TEST_KEY } = process.env
 
 
+  // console.log("hostname is: ", window.location.hostname );
+  // if (window.location.hostname == (liveServer || localHost) ) {
+  //   TEST_KEY = config.TEST_KEY;  
+  //   } else {
+  //     TEST_KEY = "NO NO NO"; // process.env
+  //   }
+  //   console.log("TEST KEY is : ", TEST_KEY );
+
+
 exports.handler = async (event, context) => {
   try {
     const subject = TEST_KEY // for working published version
     // const subject = event.queryStringParameters.name || "World at large"
 return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
+// return { statusCode: 200, body }
 
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
