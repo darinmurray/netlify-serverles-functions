193aeff3a08ff1db5bff25a1598eb1ad25f8a865
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-22 16:18:47 -0600

    garbage

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,26 +1,16 @@
 
-
-
-  const { TEST_KEY } = process.env
-
-
-  // console.log("hostname is: ", window.location.hostname );
-  // if (window.location.hostname == (liveServer || localHost) ) {
-  //   TEST_KEY = config.TEST_KEY;  
-  //   } else {
-  //     TEST_KEY = "NO NO NO"; // process.env
-  //   }
-  //   console.log("TEST KEY is : ", TEST_KEY );
+const { TEST_KEY } = process.env
 
 
 exports.handler = async (event, context) => {
   try {
-    const subject = "TEST_KEY" // for working published version
-    // const subject = event.queryStringParameters.name || "World at large"
+    const subject = TEST_KEY // for working published version
+  // const subject = event.queryStringParameters.name || "World at large"
 return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
 // return { statusCode: 200, body }
 
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
-  }
+  } 
+
 }
--- public/script.js
+++ public/script.js
@@ -1,11 +1,6 @@
 window.onload = function() {
 
-  // netlify = "affectionate-poincare-72fa68.netlify.app"
-  // localHost = "localhost"
-  // liveServer = "127.0.0.1"
 
-
-console.log("hostname is: ", window.location.hostname );
   const message = document.getElementById("message")
   document.getElementById("button").addEventListener("click", () => {
     // console.log("hello", TEST_KEY)
@@ -22,7 +17,7 @@ console.log("hostname is: ", window.location.hostname );
 // ==========   Generate a Random Alpha Character   ============= //
 randoAlpha = String.fromCharCode(65+Math.floor(Math.random() * 26)); 
 
-  // =======   Generate a Random Int between min & max   ========== //
+// =======   Generate a Random Int between min & max   ========== //
 // getRandomInt(1, 10)*100   for increments less than one second
 function getRandomInt(min, max) { 
   return Math.round((min - 0.5) + Math.random() * (max - min + 1));
