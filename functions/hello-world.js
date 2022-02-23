4c31ab3bacf54878a23e7f2710094257cd919b4d
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-22 19:40:16 -0600

    grasping at straws...

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -2,15 +2,25 @@
 const { TEST_KEY } = process.env
 
 
-exports.handler = async (event, context) => {
-  try {
-    const subject = TEST_KEY // for working published version
-  // const subject = event.queryStringParameters.name || "World at large"
-return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
-// return { statusCode: 200, body }
+// exports.handler = async (event, context) => {
+//   try {
+//     const subject = TEST_KEY // for working published version
+//   // const subject = event.queryStringParameters.name || "World at large"
+// return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
+// // return { statusCode: 200, body }
 
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  } 
+//   } catch (err) {
+//     return { statusCode: 500, body: err.toString() }
+//   } 
 
-}
+// }
+
+
+exports.handler = async (event) => {
+  const secret_key = TEST_KEY // for working published version
+  const query_name = event.queryStringParameters.name || "World at large"
+  return {
+    statusCode: 200,
+    body: `Hello ${query_name} and ${secret_key}`,
+  };
+};
---
+++ functions/search.js
@@ -0,0 +1,38 @@
+const qs = require('querystring');
+const fetch = require("node-fetch");
+// const fetch = require('node-fetch');
+// const fetch = import('node-fetch');
+// const fetch = import fetch from 'node-fetch'
+// require('whatwg-fetch')
+// import 'whatwg-fetch'
+
+exports.handler = async (event) => {
+  const { query } = qs.parse(event.body);
+
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?query=${query}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+      },
+    }
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+
+  const firstResult = response.results[0];
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: `
+      <img
+        src="${firstResult.urls.regular}"
+        alt="${firstResult.alt_description}"
+      />
+    `,
+  };
+};
