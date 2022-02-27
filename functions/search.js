9b00bfc822d130e6f01cd8c720393e9b3b82bea9
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-26 22:40:49 -0600

    expanding js functionality

--- functions/search.js
+++ functions/search.js
@@ -4,35 +4,36 @@ const fetch = require('node-fetch');
 
 
 exports.handler = async (event) => { 
-  // const query = event.body.split('=')[1] //"blue" //qs.parse(event.body);
-  const query = event.queryStringParameters.query //"blue" //qs.parse(event.body);
+
+  const query = event.body.split('=')[1] //"blue" //qs.parse(event.body);
+  // const query = event.queryStringParameters.query //"blue" //qs.parse(event.body);
   const params = event.queryStringParameters.query // this is in fact, empty
   // const M_W_API = "07bf658c-2b15-4b71-9fa5-a12e8aaa0f79"
   const response = await fetch(  
     `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=${process.env.M_W_API}`,
     // `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`,
-    // `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key="+M_W_API`,
+    // `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=${process.env.M_W_API}`,
     // `https://api.unsplash.com/search/photos?query=${query}`,
     {
       method: 'GET',
       mode: 'no-cors',
       headers: {
         // Authorization: `Client-ID qHPEkhMngfL7c9A5KoAhYbWXv7OcuFoWIEg6p7p_I-0`,
-        Authorization: `Client-ID ${process.env.UNSPLASH_API_TOKEN}`,
+        Authorization: `Client-ID ${process.env.M_W_API}`,
       },
     }
   )
     .then((response) => response.json())
     .catch((error) => console.error(error));
-
   const firstResult = response //.results[0];
-
+  console.log("Query is ", query);
+  console.log("FIrst REsult is ", firstResult);
+  console.log("Jusrt before Return ", response);
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/html',
     },
-    
     body: `
     Results for :${query} and QSP: ${params}
     </br></br>
@@ -41,6 +42,7 @@ exports.handler = async (event) => {
     ` + event.queryStringParameters.query ,
     // `+ JSON.stringify(query.body, undefined, 4),
   };
+  
 };
 
 
