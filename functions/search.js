2c5687f9bbb9214642ebe1ecea78e6ea4faae6ed
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-27 12:43:58 -0600

    refine results

--- functions/search.js
+++ functions/search.js
@@ -1,12 +1,19 @@
 const fetch = require("node-fetch");
 
 // const params = event.queryStringParameters 
+// this used to include context, but it screwed up queryStringParameters
+// exports.handler = async (event, context) => {
+exports.handler = async event => {
+    console.log("this should be the query string params",event.queryStringParameters);
+    // let supply = [ "red","green","dog","cat","car","rocket", ]
+    // const random = Math.floor(Math.random() * supply.length);
 
-exports.handler = async (event, context) => {
-    let supply = [ "red","green","dog","cat","car","rocket", ]
-    const random = Math.floor(Math.random() * supply.length);
-    // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=${process.env.M_W_API}`;
-    const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/turtle?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
+    const querystring = event.queryStringParameters;
+    const searchable = querystring.searchable || 'fail';
+    // let searchable = supply[random]
+
+    const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchable}?key=${process.env.M_W_API}`;
+    // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/turtle?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
     try {
         const jokeStream = await fetch(url, {
             mode: 'no-cors',
--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,19 @@
 // const fetch = require("node-fetch");
-
 //window.onload = function () { 
- 
-let supply = [ "red","green","dog","cat","car","rocket", ]   
-const random = Math.floor(Math.random() * supply.length);
+// let supply = [ "red","green","dog","cat","car","rocket", ]   
+// const random = Math.floor(Math.random() * supply.length);
+
 
 const getRandomDadJoke = async (event) => {
-const random = Math.floor(Math.random() * supply.length);
+    console.log("getRandomDadJoke:",event);
+    let supply = [ "lizard","lion","ukraine","tree","car","rocket", ]   
+    const random = Math.floor(Math.random() * supply.length);
 
-/* **************** for NON serverles ***************** */
-const url = "/.netlify/functions/search";
+/* ****************  for S E R V E R L E S S  ***************** */
+const searchable = supply[random];
+const url = "/.netlify/functions/search?searchable="+searchable ;
 const jokeStream = await fetch(url);
-/* **************** E N D ***************** */
-
-
+/* ****************  E N D  ***************** */
 
 /* **************** for NON serverles ***************** */
 // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=${process.env.M_W_API}`;
@@ -26,30 +26,29 @@ const jokeStream = await fetch(url);
 /* **************** E N D ***************** */
 
 const jsonJoke = await jokeStream.json();
-
 // const joke = jsonJoke //.joke; // this should change do shortDef or something...
-const joke = jsonJoke[0].shortdef
-console.log(joke);
+const joke = jsonJoke //[0].shortdef
+// console.log(joke);
 return joke;
 };
 
-
-const displayJoke = (joke) => {
+const displayJoke = (joke, searchable) => {
+    console.log(joke);
     const h1 = document.querySelector("h1");
-    h1.textContent = getRandomInt(100, 999)+joke;
+    h1.textContent = getRandomInt(100, 999)+": "+joke[0].shortdef;
+    h1.prepend(joke[0].meta.id+": ");
 }
-
 
 const refreshJoke = async() => {
     const joke = await getRandomDadJoke();
     displayJoke(joke);
+    console.log("The whole joke is:", joke);
 }
 
 refreshJoke()
 
 // setInterval(alert('working'), 5000);
 // setInterval(refreshJoke, 3000);
-
 
 $(document).on("click", "#get_me" , function(e) {
     console.log("#get_me Clicked, do something");
@@ -59,7 +58,22 @@ $(document).on("click", "#get_me" , function(e) {
 
     
     
-    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // =======   Generate a Random Int between min & max   ========== //
 // getRandomInt(1, 10)*100   for increments less than one second
 function getRandomInt(min, max) { 
