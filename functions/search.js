cf578528550bcb866c3e1771a8407afbd8d74179
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-27 15:09:01 -0600

    Change all references to "Definition"

--- functions/search.js
+++ functions/search.js
@@ -1,33 +1,24 @@
 const fetch = require("node-fetch");
 
-// const params = event.queryStringParameters 
 // this used to include context, but it screwed up queryStringParameters
 // exports.handler = async (event, context) => {
 exports.handler = async event => {
-    console.log("this should be the query string params",event.queryStringParameters);
-    // let supply = [ "red","green","dog","cat","car","rocket", ]
-    // const random = Math.floor(Math.random() * supply.length);
-
     const querystring = event.queryStringParameters;
     const searchable = querystring.searchable || 'fail';
-    // let searchable = supply[random]
-
     const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchable}?key=${process.env.M_W_API}`;
     // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/turtle?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
     try {
-        const jokeStream = await fetch(url, {
+        const defStream = await fetch(url, {
             mode: 'no-cors',
             headers: {
                 Accept: 'application/json'
             }
         });
-        const jsonJoke = await jokeStream.json();
-        // const joke = jsonJoke //.joke; // this should change do shortDef or something...
-        // const joke = jsonJoke[0].shortdef
-        // console.log(joke);
+        const jsonDefinition = await defStream.json();
+
         return {
             statusCode: 200,
-            body: JSON.stringify(jsonJoke)
+            body: JSON.stringify(jsonDefinition)
         };
     } catch (err) {
         return {statusCode: 422, body: err.stack };
--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,43 @@
-// const fetch = require("node-fetch");
-//window.onload = function () { 
-// let supply = [ "red","green","dog","cat","car","rocket", ]   
-// const random = Math.floor(Math.random() * supply.length);
 
-
-const getRandomDadJoke = async (event) => {
-    console.log("getRandomDadJoke:",event);
-    let supply = [ "lizard","lion","ukraine","tree","car","rocket", ]   
+const getDefinition = async (event) => {
+    const supply = [ "lizard","lion","ukraine","tree","car","rocket", ]   
     const random = Math.floor(Math.random() * supply.length);
+    const searchable = supply[random];
 
 /* ****************  for S E R V E R L E S S  ***************** */
-const searchable = supply[random];
 const url = "/.netlify/functions/search?searchable="+searchable ;
-const jokeStream = await fetch(url);
+const defStream = await fetch(url);
 /* ****************  E N D  ***************** */
 
 /* **************** for NON serverles ***************** */
-// const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=${process.env.M_W_API}`;
-// const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
-// const jokeStream = await fetch(url, {
+// const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchable}?key=${process.env.M_W_API}`;
+// const defStream = await fetch(url, {
 //     headers: {
 //         Accept: 'application/json'
 //       }
 // });
 /* **************** E N D ***************** */
 
-const jsonJoke = await jokeStream.json();
-// const joke = jsonJoke //.joke; // this should change do shortDef or something...
-const joke = jsonJoke //[0].shortdef
-// console.log(joke);
-return joke;
+const jsonDefinition = await defStream.json();
+const definition = jsonDefinition 
+return definition;
 };
 
-const displayJoke = (joke, searchable) => {
-    console.log(joke);
+const displayDef = (definition) => {
     const h1 = document.querySelector("h1");
-    h1.textContent = getRandomInt(100, 999)+": "+joke[0].shortdef;
-    h1.prepend(joke[0].meta.id+": ");
+    h1.textContent = definition[0].meta.id+": -"+getRandomInt(100, 999)+"- : "+definition[0].shortdef;
 }
 
-const refreshJoke = async() => {
-    const joke = await getRandomDadJoke();
-    displayJoke(joke);
-    console.log("The whole joke is:", joke);
+const refreshDef = async() => {
+    const definition = await getDefinition();
+    displayDef(definition);
 }
 
-refreshJoke()
 
-// setInterval(alert('working'), 5000);
-// setInterval(refreshJoke, 3000);
+refreshDef()
 
 $(document).on("click", "#get_me" , function(e) {
-    console.log("#get_me Clicked, do something");
-    refreshJoke()
+    refreshDef()
 });
     
 
