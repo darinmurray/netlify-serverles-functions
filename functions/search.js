1fc864dd25b464d329e72b1877dfd4432a54e307
Darin Murray <97316685+darinmurray@users.noreply.github.com>
2022-02-26 23:07:04 -0600

    extracting to lambda

--- functions/search.js
+++ functions/search.js
@@ -1,85 +1,24 @@
-// const querySTringTHing = require('querystring');
-const fetch = require('node-fetch');
-// import fetch from 'node-fetch';
+const fetch = require("node-fetch");
 
-
-exports.handler = async (event) => { 
-
-  const query = event.body.split('=')[1] //"blue" //qs.parse(event.body);
-  // const query = event.queryStringParameters.query //"blue" //qs.parse(event.body);
-  const params = event.queryStringParameters.query // this is in fact, empty
-  // const M_W_API = "07bf658c-2b15-4b71-9fa5-a12e8aaa0f79"
-  const response = await fetch(  
-    `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=${process.env.M_W_API}`,
-    // `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`,
-    // `https://www.dictionaryapi.com/api/v3/references/sd2/json/${query}?key=${process.env.M_W_API}`,
-    // `https://api.unsplash.com/search/photos?query=${query}`,
-    {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        // Authorization: `Client-ID qHPEkhMngfL7c9A5KoAhYbWXv7OcuFoWIEg6p7p_I-0`,
-        Authorization: `Client-ID ${process.env.M_W_API}`,
-      },
+exports.handler = async (event, context) => {
+    const random = Math.floor(Math.random() * supply.length);
+    // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=${process.env.M_W_API}`;
+    const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
+    try {
+        const jokeStream = await fetch(url, {
+            headers: {
+                Accept: 'application/json'
+            }
+        });
+        const jsonJoke = await jokeStream.json();
+        // const joke = jsonJoke //.joke; // this should change do shortDef or something...
+        // const joke = jsonJoke[0].shortdef
+        // console.log(joke);
+        return {
+            statusCode: 200,
+            body: JSON.stringigy(jsonJoke)
+        };
+    } catch (err) {
+        return {statusCode: 422, body: err.stack };
     }
-  )
-    .then((response) => response.json())
-    .catch((error) => console.error(error));
-  const firstResult = response //.results[0];
-  console.log("Query is ", query);
-  console.log("FIrst REsult is ", firstResult);
-  console.log("Jusrt before Return ", response);
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: `
-    Results for :${query} and QSP: ${params}
-    </br></br>
-    Some value is: ${firstResult[0].shortdef}
-</br></br>
-    ` + event.queryStringParameters.query ,
-    // `+ JSON.stringify(query.body, undefined, 4),
-  };
-  
 };
-
-
-// "body":"query=dog",
-
-
-
-// exports.handler = async (event) => {
-//   const { query } = "green" //qs.parse(event.body);
-
-//   const response = await fetch(
-//     `https://api.unsplash.com/search/photos?query=${query}`,
-//     {
-//       method: 'GET',
-//       headers: {
-//         // Authorization: `Client-ID qHPEkhMngfL7c9A5KoAhYbWXv7OcuFoWIEg6p7p_I-0`,
-//         Authorization: `Client-ID qHPEkhMngfL7c9A5KoAhYbWXv7OcuFoWIEg6p7p_I-0`,
-//       },
-//     }
-//   )
-//     .then((response) => response.json())
-//     .catch((error) => console.error(error));
-
-//   const firstResult = response.results[0];
-
-//   return {
-//     statusCode: 200,
-//     headers: {
-//       'Content-Type': 'text/html',
-//     },
-    
-//     body: `
-//     Token is:${process.env.UNSPLASH_API_TOKEN}
-//       <img
-//         src= "${firstResult.urls.regular}"
-//         alt="${firstResult.alt_description}"
-//       />
-//     `,
-//   };
-// };
--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,24 @@
+// const { default: fetch } = require("node-fetch");
+
 window.onload = function () { 
  
 let supply = [ "red","green","dog","cat","car","rocket", ]   
 const random = Math.floor(Math.random() * supply.length);
-// console.log(random, supply[random]);
-
-
 
 const getRandomDadJoke = async (event) => {
 const random = Math.floor(Math.random() * supply.length);
 // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=${process.env.M_W_API}`;
-const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
-const jokeStream = await fetch(url, {
-    headers: {
-        Accept: 'application/json'
-      }
-});
+// const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${supply[random]}?key=07bf658c-2b15-4b71-9fa5-a12e8aaa0f79`;
+// const jokeStream = await fetch(url, {
+//     headers: {
+//         Accept: 'application/json'
+//       }
+// });
+
+const url = "./netlify/functions/search";
+const jokeStream = await fetch(url);
 const jsonJoke = await jokeStream.json();
+
 // const joke = jsonJoke //.joke; // this should change do shortDef or something...
 const joke = jsonJoke[0].shortdef
 console.log(joke);
