1d07f34ff32ae16506d88e5c24ce61dd0551c119
darinmurray <webdev.dtm@gmail.com>
2022-02-28 11:32:28 -0600

    build out word lambda

--- functions/word.js
+++ functions/word.js
@@ -1,5 +1,52 @@
+const fetch = require("node-fetch");
+// this used to include context, but it screwed up queryStringParameters
+// exports.handler = async (event, context) => {
+exports.handler = async event => {
+    const querystring = event.queryStringParameters;
+    const searchable = querystring.searchable || 'democracy';
+    // const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${searchable}?key=${process.env.M_W_API}`;
+    // ${process.env.M_W_API}
+    const url = `https://wordsapiv1.p.rapidapi.com/words/?random=true`;
+    try {
+        const randoWordStream = await fetch(url, {
+            mode: 'no-cors',
+            method: "GET",
+            headers: {
+                Accept: 'application/json',
+                "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+                "x-rapidapi-key": process.env.WORDS_KEY  // see config.js
+            }
+        });
+        const randoWord = await randoWordStream.json();
 
-exports.handler = async () => ({
-    statusCode: 200,
-    body: 'bleep blop bleep blerb bloobob...',
-  });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(randoWord)
+        };
+    } catch (err) {
+        return {statusCode: 422, body: err.stack };
+    }
+};
+
+
+
+
+// exports.handler = async () => ({
+//     statusCode: 200,
+//     body: 'bleep blop bleep blerb bloobob...',
+//   });
+
+//       const settings = {
+//     	"async": true,
+//     	"crossDomain": true,
+//       "url": "https://wordsapiv1.p.rapidapi.com/words/?random=true",
+//     	"method": "GET",
+//     	"headers": {
+//     		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+//     		"x-rapidapi-key": WORDS_API // see config.js
+//     	}
+//     };
+    
+//     $.ajax(settings).done(function (response) {
+//       console.log(`%c=> WORDS AIP random response: `, "color:ogoldenrod", response);
+//     });
